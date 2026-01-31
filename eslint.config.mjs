af7cc116551bb0d9b92193fdbe9f7decea1e68a6
NULL-Header <poaoq.poaoq@gmail.com>
2026-01-31 16:26:24 +0900

    fix wrong config and format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,3 @@
-// eslint.config.mjs
-import nuxt from "@nuxt/eslint-config";
+import { createConfigForNuxt } from '@nuxt/eslint-config';
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
-export default [
-  ...nuxt,
-  {
-    rules: {
-      //   "vue/multi-word-component-names": "off",
-      //   "@typescript-eslint/no-explicit-any": "off",
-    },
-  },
-];
+export default createConfigForNuxt();
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('prettier').Config} */
 export default {
-  semi: false,
+  semi: true,
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 100,
   tabWidth: 2,
   arrowParens: 'always',
-}
+};
