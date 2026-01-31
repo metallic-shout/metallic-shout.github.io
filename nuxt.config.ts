b10e7f5745104b330e62d0f78c4fc4dbe0231954
NULL-Header <poaoq.poaoq@gmail.com>
2026-01-31 19:59:29 +0900

    add config for module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
+import { fileURLToPath } from 'node:url';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  alias: {
+    '@root': fileURLToPath(new URL('./', import.meta.url)),
+  },
+  typescript: {
+    tsConfig: {
+      include: ['../lib/**/*.ts', '../modules/**/*', '../data/**/*'],
+    },
+  },
 });
