b6cad81b72441bde6d4dbd166b4cffd80c929969
NULL-Header <poaoq.poaoq@gmail.com>
2026-02-01 15:02:20 +0900

    remove unused logic

--- modules/elements-gen/index.ts
+++ modules/elements-gen/index.ts
@@ -1,7 +1,7 @@
 // modules/elements-gen/module.ts
 import { addTemplate, createResolver, defineNuxtModule, useLogger } from '@nuxt/kit';
 import { string2DoubleStrucks } from './string2double-struck';
-import ELEMENTS from './elements.soruce';
+import ELEMENTS from './elements.source';
 
 export default defineNuxtModule({
   meta: {
@@ -13,26 +13,22 @@ export default defineNuxtModule({
     const logger = useLogger('elements-gen');
     const { resolve } = createResolver(import.meta.url);
 
-    const sourcePath = resolve(nuxt.options.rootDir, 'data/elements.source.ts');
-    nuxt.options.watch.push(sourcePath);
-
     const buildMap = async () => {
       if (!Array.isArray(ELEMENTS)) {
-        throw new Error(`[elements-gen] ELEMENTS must be an array in ${sourcePath}`);
+        throw new Error(`[elements-gen] ELEMENTS must be an array.`);
       }
       const map: Record<string, string> = {};
 
       for (const [_, name] of ELEMENTS as [string, string][]) {
         const res = string2DoubleStrucks(name);
         if (!res.ok) {
-          throw new Error(`[elements-gen] ELEMENTS must be an alphabet! in ${sourcePath}`);
+          throw new Error(`[elements-gen] ELEMENTS must be an alphabet!`);
         }
         map[name] = res.value;
       }
       return map;
     };
 
-    // (A) JSON を生成：Vite/Nitro が import 時にバンドルへ埋め込むので「通信不要」
     addTemplate({
       filename: 'elements/elements.generated.json',
       getContents: async () => {
