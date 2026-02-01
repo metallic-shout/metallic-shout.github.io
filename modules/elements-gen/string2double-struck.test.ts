b320a2cb266b41e3f74cb0ce2a45b8d741ad2cce
NULL-Header <poaoq.poaoq@gmail.com>
2026-02-01 15:11:08 +0900

    add test

---
+++ modules/elements-gen/string2double-struck.test.ts
@@ -0,0 +1,19 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+
+import { string2DoubleStrucks } from './string2double-struck';
+
+test('アルファベットはダブルストラック体に変換される', () => {
+  const res = string2DoubleStrucks('ABa');
+
+  assert.equal(res.ok, true);
+  assert.equal(res.value, '\u{1D538}\u{1D539}\u{1D552}');
+});
+
+test('アルファベット以外が含まれる場合はエラー', () => {
+  const res = string2DoubleStrucks('A1');
+
+  assert.equal(res.ok, false);
+  assert.equal(res.error.kind, 'ArgStringNotAlphabetError');
+});
+
