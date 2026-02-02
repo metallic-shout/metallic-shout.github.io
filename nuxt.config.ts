9c34df44c7db4b93bea3c807186552ef33f53153
NULL-Header <poaoq.poaoq@gmail.com>
2026-02-02 16:48:40 +0900

    remove alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
-import { fileURLToPath } from 'node:url';
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  alias: {
-    '@root': fileURLToPath(new URL('./', import.meta.url)),
-  },
   typescript: {
     tsConfig: {
       include: ['../lib/**/*.ts', '../modules/**/*', '../data/**/*'],
