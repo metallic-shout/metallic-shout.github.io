8f586a679791d9399e725f230b0609559128f398
NULL-Header <poaoq.poaoq@gmail.com>
2026-02-02 16:47:11 +0900

    test too

--- lib/string2double-struck.test.ts
+++ lib/string2double-struck.test.ts
@@ -1,16 +1,16 @@
 import assert from 'node:assert/strict';
 import test from 'node:test';
 
-import { string2DoubleStrucks } from '../../lib/string2double-struck';
+import { string2DoubleStrucks } from './string2double-struck';
 
-test('アルファベットはダブルストラック体に変換される', () => {
+test('アルファベットがダブルストラック体に変換される', () => {
   const res = string2DoubleStrucks('ABa');
 
   assert.equal(res.ok, true);
   assert.equal(res.value, '\u{1D538}\u{1D539}\u{1D552}');
 });
 
-test('アルファベット以外が含まれる場合はエラー', () => {
+test('アルファベット以外が混ざるとエラーになる', () => {
   const res = string2DoubleStrucks('A1');
 
   assert.equal(res.ok, false);
