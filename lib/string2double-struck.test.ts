2a1f02e3e1f4b75340e13af19fc02b1927ee7376
NULL-Header <poaoq.poaoq@gmail.com>
2026-02-03 10:51:50 +0900

    fix typeguard

--- lib/string2double-struck.test.ts
+++ lib/string2double-struck.test.ts
@@ -6,6 +6,9 @@ it('アルファベットがダブルストラック体に変換される', () =
   const res = string2DoubleStrucks('ABa');
 
   expect(res.ok).toBe(true);
+  if (!res.ok) {
+    throw new Error('expected ok result');
+  }
   expect(res.value).toBe('\u{1D538}\u{1D539}\u{1D552}');
 });
 
@@ -13,5 +16,8 @@ it('アルファベット以外が混ざるとエラーになる', () => {
   const res = string2DoubleStrucks('A1');
 
   expect(res.ok).toBe(false);
+  if (res.ok) {
+    throw new Error('expected err result');
+  }
   expect(res.error.kind).toBe('ArgStringNotAlphabetError');
 });
