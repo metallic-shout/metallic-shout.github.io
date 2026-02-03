b52b049881b704c08b303f176db12f6d8d859fd6
NULL-Header <poaoq.poaoq@gmail.com>
2026-02-04 07:42:41 +0900

    add css config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,16 @@ export default <Partial<Config>>{
   content: [],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'rgb(var(--background) / <alpha-value>)',
+        foreground: 'rgb(var(--foreground) / <alpha-value>)',
+        card: 'rgb(var(--card) / <alpha-value>)',
+        'card-foreground': 'rgb(var(--card-foreground) / <alpha-value>)',
+        border: 'rgb(var(--border) / <alpha-value>)',
+        ring: 'rgb(var(--ring) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 };
